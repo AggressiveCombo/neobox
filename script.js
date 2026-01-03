ab35104fd9fb191f94b74da6bbf7a515d3a354c4
Aggressive Combo <80444661+AggressiveCombo@users.noreply.github.com>
2026-01-03 18:32:03 +0100

    refactor: format with Prettier again
    Markdown was cleaned up by adding blank lines and modifying some double whitespaces by Prettier. Prettier previously put DOCTYPE in lowercase, and now it's in uppercase. I'm not sure why. JS will now use 2 space indents until there's a coding style guide.

--- script.js
+++ script.js
@@ -2,6 +2,6 @@
 
 // Console startup GitHub easter-egg
 console.log(
-    "%cCurious? Go to https://www.github.com/AggressiveCombo/neobox",
-    "color: hsl(0, 0%, 65%); background-color: hsl(0, 0%, 0%);"
+  "%cCurious? Go to https://www.github.com/AggressiveCombo/neobox",
+  "color: hsl(0, 0%, 65%); background-color: hsl(0, 0%, 0%);"
 );
