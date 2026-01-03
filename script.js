fc90b4c985d3e61f1931e58b6d09e0ef0be857fb
Aggressive Combo <adammasek08@gmail.com>
2026-01-03 18:05:01 +0100

    feat: add comment about the easter egg repo link in console output

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// Console startup GitHub easter-egg
 console.log(
     "%cCurious? Go to https://www.github.com/AggressiveCombo/neobox",
     "color: hsl(0, 0%, 65%); background-color: hsl(0, 0%, 0%);"
